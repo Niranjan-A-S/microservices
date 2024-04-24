6b8f2eeefd00124f7b535f07e7ff1c0bb6695e20
Niranjan Avanippilli Santhosh <niranjan.santhosh@agilepoint.com>
2024-04-24 18:23:38 +0530

    chore: add dummy controllers

---
+++ monolithic/src/controllers/customer.ts
@@ -0,0 +1,27 @@
+import { RequestHandler } from "express";
+
+const signup: RequestHandler = async (req, res, next) => {
+
+};
+
+const login: RequestHandler = async (req, res, next) => {
+
+};
+
+const addAddress: RequestHandler = async (req, res, next) => {
+
+};
+
+const getProfile: RequestHandler = async (req, res, next) => {
+
+};
+
+const getShoppingDetails: RequestHandler = async (req, res, next) => {
+
+};
+
+const getWishlist: RequestHandler = async (req, res, next) => {
+
+};
+
+export { signup, addAddress, getProfile, getWishlist, login, getShoppingDetails };
